4e74141b51aefb2ab1d061b56d0b85124c97a975
Aakash Verma <4k45hr0ck5007@gmail.com>
2020-05-02 13:53:00 +0530

    added Post model

---
+++ my_site/blog/models.py
@@ -0,0 +1,33 @@
+from django.db import models
+
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+class Post(models.Model):
+    STATUS_CHOICES=(
+            ('draft','Draft'),
+            ('published','Published')
+        )
+    title = models.CharField(max_length=250)
+    #short label to be used for URLs
+    slug  = models.SlugField(max_length=250,
+        unique_for_date='publish')
+    author = models.ForeignKey(User, on_delete=models.CASCADE,
+                               related_name="blog_posts")#many_to_one relationship with user model of django
+    body = models.TextField()
+    #time when post published
+    publish = models.DateTimeField(default = timezone.now)
+    #time when post was created
+    created = models.DateTimeField(auto_now_add=True)
+    #time when post was updated
+    updated = models.DateTimeField(auto_now=True)
+    status  = models.CharField(max_length=10,
+                               choices=STATUS_CHOICES,
+                               default='draft')
+    class Meta:
+        #post ordering in descending using publish field
+        ordering = ('-publish',)
+
+    def __str__(self):
+        #default human-readable representation of the object
+        return self.title
