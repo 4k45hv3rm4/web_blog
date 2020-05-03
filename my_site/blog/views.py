e5283cae570fa22a751bd6b4bfa4c891c3aed6e8
Aakash Verma <4k45hr0ck5007@gmail.com>
2020-05-03 09:13:07 +0530

    added pagination to the posts_list

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 from .models import Post
 from django.shortcuts import render, get_object_or_404
 
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post_list.html', {'posts':posts})
+
+    paginator = Paginator(posts, 3) #3 posts per page
+    page = request.GET.get('page')
+
+    try:
+        posts=paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return render(request,
+                    'blog/post_list.html',
+                    {'page':page,
+                    'posts':posts})
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post,
